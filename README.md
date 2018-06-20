29dfd3be00be668ced38b361871c998234f92062
David <34197755+david722@users.noreply.github.com>
2018-06-20 19:52:40 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic dating app
---
+++ app/data/friends.js
@@ -0,0 +1,120 @@
+
+
+
+var friendsArr = [
+    {
+      "name": "Dash",
+      "photo": "https://vignette.wikia.nocookie.net/superheroes/images/2/20/Dash.jpg/revision/latest?cb=20141104103843",
+      "scores": [
+          "4",
+          "2",
+          "5",
+          "1",
+          "3",
+          "3",
+          "1",
+          "5",
+          "5",
+          "5"
+        ]
+    },
+    {
+        "name": "Ralph",
+        "photo": "https://vignette.wikia.nocookie.net/disney/images/d/da/Wreck_it_Ralph_pose_transparent.png/revision/latest?cb=20151202214156",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "4",
+            "3",
+            "3",
+            "5",
+            "5",
+            "4",
+            "1"
+        ]
+      },
+      {
+        "name": "Dug",
+        "photo": "https://thedisneydivadotcom.files.wordpress.com/2013/12/dug-from-up.jpg",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+      },
+      {
+        "name": "Mike",
+        "photo": "https://vignette.wikia.nocookie.net/disney/images/8/86/Mike-wazowski-10.png/revision/latest?cb=20180213022457",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "1",
+            "4",
+            "4"
+        ]
+      },
+      {
+        "name": "Bruce",
+        "photo": "https://vignette.wikia.nocookie.net/disney/images/a/a5/Bruce-FN.png/revision/latest?cb=20131111065729",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "1",
+            "1"
+        ]
+      },
+      {
+        "name": "Jessie",
+        "photo": "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-y1ys73_7dd5c0b7.jpeg",
+        "scores": [
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "4",
+            "4",
+            "1",
+            "2",
+            "5"
+        ]
+      },
+      {
+        "name": "Moana",
+        "photo": "https://pbs.twimg.com/profile_images/819955513718779905/DY4UfMVj_400x400.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "4",
+            "3",
+            "3",
+            "4",
+            "4",
+            "2"
+        ]
+      }
+  ];
+
+  module.exports = friendsArr;
+
